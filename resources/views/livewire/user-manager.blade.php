c46b0620ec9a2f9b4c281313dda38301c48abbea
doni <doniaries@gmail.com>
2025-09-10 22:46:39 +0700

    perbaikan reveabled

--- resources/views/livewire/user-manager.blade.php
+++ resources/views/livewire/user-manager.blade.php
@@ -204,7 +204,7 @@
                             <div class="relative">
                                 <input :type="showPassword ? 'text' : 'password'" wire:model="password" id="password"
                                        class="w-full px-3 py-2 pr-10 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                                       @if(!$isEditMode) required @endif>
+                                       @if(!$isEditMode) required @endif autocomplete="new-password">
                                 <button type="button" 
                                         @click="showPassword = !showPassword"
                                         class="absolute inset-y-0 right-0 px-3 flex items-center text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
